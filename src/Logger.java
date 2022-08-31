d784cd3dece6527f785c4b8da4611b2d9519469a
xuxu2333 <hrx@hrxdeMacBook-Pro.local>
2022-08-31 15:08:02 +1000

    Add print

--- src/Logger.java
+++ src/Logger.java
@@ -6,6 +6,8 @@ public class Logger {
     public static void main(String[] args) {
 
         ArrayList<String> list = new ArrayList<String>();
+
+        System.out.println("Hi");
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNextLine()) {
             list.add(scanner.nextLine());
